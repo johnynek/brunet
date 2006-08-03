ee7b1cc64ac23fe07f2ac1853cb6e730ba3d51ad
root@localhost.localdomain <root@localhost.localdomain>
2006-08-03 12:30:06 -0400

    Updated dns.py and moved sample-config files - David Wolinsky

--- src/dns/dns.py
+++ src/dns/dns.py
@@ -10,19 +10,26 @@ class DNSRequestHandler (DatagramRequestHandler):
     self.packet, self.socket = self.request
     self.data = array.array('B', self.packet)
     self.ip = [0, 0, 0, 0]
+    current = 12
+    while self.data[current] != 0:
+      current += self.data[current] + 1
+    self.qtype = (int(self.data[current+1]) << 8) + int(self.data[current+2])
 
   def handle(self):
     bad = 1
     if self.check_header() == 1:
-      #It has to be one word
-      if self.data[self.data[12] + 1] == 0:
-        if self.check_name_node():
-          bad = 0
+      if self.check_name_node():
+        bad = 0
+      elif self.check_name_arpa():
+        bad = 0
 
     if bad == 0:
-      if(self.ip[1] < 256 and self.ip[2] < 256 and self.ip[3] < 255 \
-        and self.ip [3] > 0):
-        self.add_answer()
+      if(self.ip[0] == 10 and self.ip[1] > 127 and self.ip[1] < 256 and \
+        self.ip[2] < 256 and self.ip[3] < 255 and self.ip [3] > 0):
+        if self.qtype == 12:
+          self.add_answer_ptr()
+        else:
+          self.add_answer_host()
       else:
         bad = 1
 
@@ -59,7 +66,28 @@ class DNSRequestHandler (DatagramRequestHandler):
         return True
     return False
 
-  def add_answer(self):
+  def check_name_arpa(self):
+    bad = 1
+    current = 12
+    parsed_data = []
+    while self.data[current] != 0:
+      temp = ""
+      for i in (range(1,self.data[current]+1)):
+        temp += chr(self.data[i + current])
+      parsed_data.append(temp)
+      current += self.data[current] + 1
+
+    if len(parsed_data) > 5:
+      for i in range(4):
+        if(parsed_data[i].isdigit == False):
+          break;
+        else:
+          self.ip[(i-3)*-1] = int(parsed_data[i])
+        if (i == 3):
+          return True
+    return False
+
+  def add_answer_host(self):
     #We now have an answer
     self.data[7] = 1
     #We are the Authoritative Answer
@@ -67,7 +95,7 @@ class DNSRequestHandler (DatagramRequestHandler):
     #Name - point (pointer = 12) to the previous string (offset = 12)
     self.data.append(0xC0)
     self.data.append(12)
-    #Type - Hostname
+    #Type
     self.data.append(0)
     self.data.append(1)
     #Class - Internet
@@ -75,7 +103,7 @@ class DNSRequestHandler (DatagramRequestHandler):
     self.data.append(1)
     #TTL - Max
     for i in range(4):
-      self.data.append(99)
+      self.data.append(127)
     #RDLENGTH
     self.data.append(0)
     self.data.append(4)
@@ -85,6 +113,42 @@ class DNSRequestHandler (DatagramRequestHandler):
     self.data.append(self.ip[2])
     self.data.append(self.ip[3])
 
+  def add_answer_ptr(self):
+    #We now have an answer
+    self.data[7] = 1
+    #We are the Authoritative Answer
+    self.data[2] = self.data[2] | 4
+
+    #Name - point (pointer = 12) to the previous string (offset = 12)
+    self.data.append(0xC0)
+    self.data.append(12)
+    #Type
+    self.data.append(0)
+    self.data.append(12)
+    #Class - Internet
+    self.data.append(0)
+    self.data.append(1)
+    #TTL - Max
+    for i in range(4):
+      self.data.append(127)
+    temp = "C"
+    for i in range(1,4):
+     if self.ip[i] < 10:
+       temp += "00"
+     elif self.ip[i] < 100:
+       temp += "0"
+     temp += str(self.ip[i])
+    domainname = "ipop"
+    #RDLENGTH
+    self.data.append(0)
+    self.data.append(len(temp) + len(domainname) + 3)
+    self.data.append(len(temp))
+    for i in range(len(temp)):
+      self.data.append(ord(temp[i]))
+    self.data.append(len(domainname))
+    for i in range(len(domainname)):
+      self.data.append(ord(domainname[i]))
+    self.data.append(0)
 
 def main():
   server = DNSServer()
