dc7993616528de1ae6d3930f92113c18d5cd24b9
aganguly@aganguly-laptop <aganguly@aganguly-laptop>
2006-10-25 23:04:05 -0400

    There is a bug in mono probably, with IPAddress(Byte[]) constructor. We have a hack to work with currently.

--- src/ipop/IPPacketParser.cs
+++ src/ipop/IPPacketParser.cs
@@ -6,19 +6,40 @@ namespace Ipop {
     readonly static int SRC_ADDR_START = 12;
     //return the destination address
     public static IPAddress DestAddr(byte[] packet) {
-      byte[] address = new byte[4];
-      for (int i = 0, k = DEST_ADDR_START; i < address.Length; i++, k++) {
-	address[i] = packet[k];
+      /* Commented out the following: no reason why shouldn't work.*/
+      //       byte[] address = new byte[4];
+      //       for (int i = 0, k = DEST_ADDR_START; i < address.Length; i++, k++) {
+      // 	System.Console.WriteLine(packet[k]);
+      // 	address[i] = packet[k];
+      //       }
+      //       return new IPAddress(address);
+      /* short term fix. */
+      string address = "";
+      int addr_len = 4;
+      int k = DEST_ADDR_START;
+      for (int i = 0; i < addr_len - 1; i++,  k++) {
+        address += packet[k].ToString() + ".";
       }
-      return new IPAddress(address);
+      address += packet[k].ToString();
+      return IPAddress.Parse(address);
     }
 
     public static IPAddress SrcAddr(byte[] packet) {
-      byte[] address = new byte[4];
-      for (int i = 0, k = SRC_ADDR_START; i < address.Length; i++, k++) {
-	address[i] = packet[k];
+      /* Commented out the following: no reason why shouldn't work.*/
+//       byte[] address = new byte[4];
+//       for (int i = 0, k = SRC_ADDR_START; i < address.Length; i++, k++) {
+// 	address[i] = packet[k];
+//       }
+//       return new IPAddress(address);
+      /* short term fix. */
+      string address = "";
+      int addr_len = 4;
+      int k = SRC_ADDR_START;
+      for (int i = 0; i < addr_len - 1; i++,  k++) {
+        address += packet[k].ToString() + ".";
       }
-      return new IPAddress(address);
+      address += packet[k].ToString();
+      return IPAddress.Parse(address);
     }
   }
 }
