1644002608083599d287b6b16043ae561bc5ed6e
aganguly@aganguly-laptop <aganguly@aganguly-laptop>
2006-10-28 12:30:50 -0400

    added a few Dht related stuff. Can specify uptime for SimpleNode after which it disconnects.

--- src/ipop/SimpleNode.cs
+++ src/ipop/SimpleNode.cs
@@ -64,9 +64,15 @@ namespace PeerVM {
 
       //configuration file 
       ReadConfiguration(args[0]);
-
-      System.Console.WriteLine("SimpleNode starting up...");
-
+      int up_time = -1;
+      if (args.Length >= 2) {
+	up_time = Int32.Parse(args[1])*1000;
+	System.Console.WriteLine("SimpleNode starting up for: {0} ms ...", up_time);
+	//_log.Debug("SimpleNode starting up for: {0} ms ...", up_time);
+      } else {
+	System.Console.WriteLine("SimpleNode starting up forever.");
+	//_log.Debug("SimpleNode starting up forever.");
+      }
       //Make a random address
       Random my_rand = new Random();
       byte[] address = new byte[Address.MemSize];
@@ -76,7 +82,7 @@ namespace PeerVM {
       //local node
       Node tmp_node = new StructuredNode(new AHAddress(address), config.brunet_namespace);
 #if IPOP_LOG
-	string listener_log = "BeginListener::::";
+      string listener_log = "BeginListener::::";
 #endif
       //Where do we listen 
       foreach(EdgeListener item in config.EdgeListeners) {
@@ -113,14 +119,33 @@ namespace PeerVM {
       } else if (config.dht_media.Equals("memory")) {
         dht = new Dht(tmp_node, EntryFactory.Media.Memory);
       }
+      System.Console.WriteLine("Calling Connect");
 #if IPOP_LOG
       _log.Debug("IGNORE");
       _log.Debug(tmp_node.Address + "::::" + DateTime.UtcNow.Ticks
                  + "::::Connecting::::" + System.Net.Dns.GetHostName() + "::::" + listener_log);
 #endif      
-      tmp_node.Connect();
-      System.Console.WriteLine("Called Connect");
-      while(true) System.Threading.Thread.Sleep(1000*60*60);
+      try {
+	tmp_node.Connect();
+	if (up_time < 0) {
+	  while(true) System.Threading.Thread.Sleep(1000*60*60);
+	} else {
+	  System.Threading.Thread.Sleep(up_time);
+	}
+      } catch (Exception e) {
+	
+      }
+      System.Console.WriteLine("Time to disconnect,,,"); 
+#if IPOP_LOG
+      _log.Debug(tmp_node.Address + "::::" + DateTime.UtcNow.Ticks
+		 + "::::Disconnecting");
+#endif
+      tmp_node.Disconnect();
+
+
+      System.Console.WriteLine("Sleep for 30000 ms,,,");
+      //additional 30 seconds for disconnect to complete
+      System.Threading.Thread.Sleep(30000);
     }
   }
 }
