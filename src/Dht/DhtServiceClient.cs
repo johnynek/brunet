b372860fd1facfca71bc7ce12c5f81be81e03ac3
Jiangyan Xu <jiangyan@ufl.edu>
2007-06-10 22:54:27 -0400

    Added Begin/Continue/EndGet as server side async Get methods to IDht and DhtAdapter. DhtAdapter serves as a base MarshalByRefObject class for common operations in all protocols which are also in IDht. SoapDht and XmlRpc inherit DhtAdapter and provide extra operations provided by their own protocol. Async operations using client side callbacks for XmlRpc and Soap are provided in IXmlRpcDht and AsyncDhtClient.

--- src/Dht/DhtServiceClient.cs
+++ src/Dht/DhtServiceClient.cs
@@ -6,13 +6,13 @@ using Brunet.Dht;
 using System;
 
 namespace Ipop {
-  public class DhtServiceClient     {
+  public class DhtServiceClient {
     public static IDht GetSoapDhtClient() {
       IDht dht = (IDht)Activator.GetObject(typeof(IDht), "http://127.0.0.1:64221/sd.rem");
       return dht;
     }
 
-    public static IDht GetXmlDhtClient() {
+    public static IXmlRpcDht GetXmlRpcDhtClient() {
       IXmlRpcDht proxy = (IXmlRpcDht)XmlRpcProxyGen.Create(typeof(IXmlRpcDht));
       proxy.Url = "http://127.0.0.1:64221/xd.rem";
       return proxy;
@@ -22,13 +22,62 @@ namespace Ipop {
   /// <summary>
   /// The XmlRpc interface for the Dht which is used on the client side
   /// </summary>
-  public interface IXmlRpcDht : IDht, CookComputing.XmlRpc.IXmlRpcProxy
-  {
+  public interface IXmlRpcDht : IDht, CookComputing.XmlRpc.IXmlRpcProxy {
     [XmlRpcMethod]
     new DhtGetResult[] Get(string key);
     [XmlRpcMethod]
     new string Create(string key, string value, string password, int ttl);
     [XmlRpcMethod]
     new string Put(string key, string value, string password, int ttl);
+    [XmlRpcMethod]
+    new string BeginGet(string key);
+    [XmlRpcMethod]
+    new DhtGetResult[] ContinueGet(string token);
+    [XmlRpcMethod]
+    new void EndGet(string token);
+    
+    
+    [XmlRpcBegin("Get")]
+    IAsyncResult BeginGetWithCallback(string key, AsyncCallback acb, object state);
+    [XmlRpcEnd]
+    DhtGetResult[] EndGet(IAsyncResult iasr);
+    [XmlRpcBegin("Create")]
+    IAsyncResult BeginCreateWithCallback(string key, string value, string password, int ttl, AsyncCallback acb, object state);
+    [XmlRpcEnd]
+    string EndCreate(IAsyncResult iasr);
+    [XmlRpcBegin("Put")]
+    IAsyncResult BeginPutWithCallback(string key, string value, string password, int ttl, AsyncCallback acb, object state);
+    [XmlRpcEnd]
+    string EndPut(IAsyncResult iasr);
+  }
+
+  public class AsyncDhtClient {
+    public delegate string PutOp(string key, string value, string password, int ttl);
+    public delegate DhtGetResult[] GetOp(string key);
+    
+    private IDht _dht;
+
+    public AsyncDhtClient(IDht dht) {
+      this._dht = dht;
+    }
+
+    public IAsyncResult BeginGetWithCallback(string key, AsyncCallback acb, object state) {
+      GetOp op = new GetOp(this._dht.Get);
+      IAsyncResult ar = op.BeginInvoke(key, acb, state);
+      return ar;
+    }
+
+    public IAsyncResult BeginPutWithCallback(string key, string value, string password, int ttl, AsyncCallback acb, object state) {
+      PutOp op = new PutOp(this._dht.Put);
+      IAsyncResult ar = op.BeginInvoke(key, value, password, ttl, acb, state);
+      return ar;
+    }
+
+    public IAsyncResult BeginCreateWithCallback(string key, string value, string password, int ttl, AsyncCallback acb, object state) {
+      PutOp op = new PutOp(this._dht.Create);
+      IAsyncResult ar = op.BeginInvoke(key, value, password, ttl, acb, state);
+      return ar;
+    }
+
   }
 }
--- src/Dht/IDht.cs
+++ src/Dht/IDht.cs
@@ -9,29 +9,25 @@ namespace Ipop {
   public interface IDht {
     /// <summary>
     /// Get Dht values
-    /// </summary>
-    /// <param name="key"></param>
-    /// <returns></returns>
+    /// </summary>    
     DhtGetResult[] Get(string key);
 
     /// <summary>
     /// Places value in Dht if it is a unique key
-    /// </summary>
-    /// <param name="key"></param>
-    /// <param name="value"></param>
-    /// <param name="password"></param>
-    /// <param name="ttl"></param>
+    /// </summary>    
     /// <returns>Returns a password if it works or NULL if it didn't</returns>
     string Create(string key, string value, string password, int ttl);
 
     /// <summary>
-    /// Places a value in Dht indexed by its key
-    /// </summary>
-    /// <param name="key"></param>
-    /// <param name="value"></param>
-    /// <param name="password"></param>
-    /// <param name="ttl"></param>
+    /// Places a value in Dht indexed by its key    
     /// <returns>The password used in this Put operation</returns>
     string Put(string key, string value, string password, int ttl);
+
+    /**
+     * @return: token for ContinueGet
+     */
+    string BeginGet(string key);
+    DhtGetResult[] ContinueGet(string token);
+    void EndGet(string token);
   }
 }
