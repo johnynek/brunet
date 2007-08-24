cb819fc0937049318a2b5db65bfbfda40edfad31
aganguly@aganguly-laptop <aganguly@aganguly-laptop>
2007-08-24 13:14:18 -0400

    Added a task scheduler. Can be used to simulate delays over function edges.

--- src/protocol/Util/Scheduler.cs
+++ src/protocol/Util/Scheduler.cs
@@ -58,12 +58,11 @@ namespace Brunet
 	if (_scheduler == null) {
 	  _scheduler = new TaskScheduler();
 	}
-	_scheduler.Start();
 	return _scheduler;
       }
     } 
 
-    protected void Start() {
+    public void Start() {
       _thread.Start();
     }
     
@@ -89,11 +88,7 @@ namespace Brunet
 	  _now_ticks = DateTime.UtcNow.Ticks;
 	  LinkedListNode<BrunetTask> next_node = _task_queue.First;
 	  if (next_node.Value.Instant > _now_ticks) {
-	    int milliseconds = (int) ((next_node.Value.Instant - _now_ticks)/10000.0);
-	    if (milliseconds < 0) {
-	      Environment.Exit(1);
-	    }
-	    //Console.WriteLine("millis:{0}", milliseconds);
+	    int milliseconds = (int) ((next_node.Value.Instant - _now_ticks)/10000);
 	    task = (BrunetTask) _in_queue.Dequeue(milliseconds,
 						  out fire);
 	  } else {
@@ -108,7 +103,6 @@ namespace Brunet
 	  
 	  task = _task_queue.First.Value;
 	  _task_queue.RemoveFirst();
-	  //Console.WriteLine("Firing event");
 	  task.Fire();
 	} else {
 	  //
@@ -177,6 +171,7 @@ namespace Brunet
     public void TestScheduler() {
       TaskScheduler scheduler = TaskScheduler.GetInstance();
       Assert.IsTrue(scheduler != null);
+      scheduler.Start();
       TestTask.Counter = 0;
       long now_ticks = DateTime.UtcNow.Ticks;
       for (int i = 0; i < 100; i++) {
