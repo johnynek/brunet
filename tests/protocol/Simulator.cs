8aaf834b91e986d7dfd447cd286ab2161ed7493b
aganguly@aganguly-laptop <aganguly@aganguly-laptop>
2007-09-01 17:55:21 -0400

    Changes to the simulator.

--- tests/protocol/Simulator.cs
+++ tests/protocol/Simulator.cs
@@ -1,6 +1,7 @@
 using System;
 using Brunet;
 using System.Collections;
+using System.Diagnostics;
 using System.Security.Cryptography;
 
 namespace Brunet {
@@ -90,93 +91,152 @@ namespace Brunet {
     Shortcut = 2
   }
   
-  public class SimNode {
-    protected class SimConnectionTable {
-      protected ArrayList _con_list;
-      protected Hashtable _ht;
-      public SimConnectionTable() {
-	_con_list = new ArrayList();
-	_ht = new Hashtable();
-      }
-      public bool Add(AHAddress address, ConType t) {
-	int idx = _con_list.BinarySearch(address);
-	if (idx < 0) {
-	  idx = ~idx;
-	  _con_list.Insert(idx, address);
-	  if (!_ht.ContainsKey(t)) {
-	    _ht[t] = new ArrayList();
-	  }
-	  ((ArrayList) _ht[t]).Add(address);
-	  return true;
-	} else {
-	  return false;
+  public class SimConnectionTable {
+    protected ArrayList _con_list;
+    protected Hashtable _ht;
+    public SimConnectionTable() {
+      _con_list = new ArrayList();
+      _ht = new Hashtable();
+    }
+    public bool Add(AHAddress address, ConType t) {
+      int idx = _con_list.BinarySearch(address);
+      if (idx < 0) {
+	idx = ~idx;
+	_con_list.Insert(idx, address);
+	if (!_ht.ContainsKey(t)) {
+	  _ht[t] = new ArrayList();
 	}
-      }
-      public AHAddress GetByIndex(int idx) {
-	if (_con_list.Count == 0) {
-	  return null;
-	}
-	idx %= _con_list.Count;
-	if( idx < 0 ) {
-	  idx += _con_list.Count;
-	}
-	return (AHAddress) _con_list[idx];
-      }
-      public int IndexOf(AHAddress address) {
-	int dest_idx = 0;
-	if (_con_list.Count == 0) {
-	  dest_idx = ~dest_idx;
-	}
-	else {
-	  dest_idx = _con_list.BinarySearch(address);
-	}
-	return dest_idx;
-      }
-      public int GetCount(ConType t) {
-	if (_ht.ContainsKey(t)) {
-	  return ((ArrayList) _ht[t]).Count;
-	} else {
-	  return 0;
-	}
-      }
-      public int GetTotalCount() {
-	return _con_list.Count;
-      }
-      public ArrayList GetConnections() {
-	return (ArrayList) _con_list.Clone();
+	((ArrayList) _ht[t]).Add(address);
+	return true;
+      } else {
+	return false;
       }
     }
+    public AHAddress GetByIndex(int idx) {
+      if (_con_list.Count == 0) {
+	return null;
+      }
+      idx %= _con_list.Count;
+      if( idx < 0 ) {
+	idx += _con_list.Count;
+      }
+      return (AHAddress) _con_list[idx];
+    }
+    public int IndexOf(AHAddress address) {
+      int dest_idx = 0;
+      if (_con_list.Count == 0) {
+	dest_idx = ~dest_idx;
+      }
+      else {
+	dest_idx = _con_list.BinarySearch(address);
+      }
+      return dest_idx;
+    }
+    public int GetCount(ConType t) {
+      if (_ht.ContainsKey(t)) {
+	return ((ArrayList) _ht[t]).Count;
+      } else {
+	return 0;
+      }
+    }
+    public int GetTotalCount() {
+      return _con_list.Count;
+    }
 
-    public AHAddress Address;
+    public ArrayList GetAllConnections() {
+      return (ArrayList) _con_list.Clone();
+    }
+
+    public ArrayList GetConnections(ConType t) {
+      if (_ht.ContainsKey(t)) {
+	return (ArrayList) ((ArrayList) _ht[t]).Clone();
+      } else {
+	return null;
+      }
+    }
+  }
+
+  public class SimNode {
+    protected static Random _rand = new Random();
+    public AHAddress LocalAddress;
     public NatType Nat;
     public int Idx;
     protected SimConnectionTable _con_table;
+    public SimConnectionTable ConnectionTable {
+      get {
+	return _con_table;
+      }
+    }
+    protected int _network_size;
+
     public SimNode(AHAddress address, NatType nat, int idx) {
-      this.Address = address;
+      this.LocalAddress = address;
       this.Nat = nat;
       this.Idx = idx;
       this._con_table = new SimConnectionTable();
     }
     
+    protected void EstimateSize() {
+      int net_size = -1;
+      BigInteger least_dist = null;
+      BigInteger greatest_dist = null;
+      int shorts = 0;
+      int structs = _con_table.GetTotalCount();
+      if( structs + 1 > net_size ) {
+	net_size = structs + 1;
+      }
+      /* We estimate the density of nodes in the address space,
+       * and since we know the size of the whole address space,
+       * we can use the density to estimate the number of nodes.
+       */
+      AHAddress local = this.LocalAddress;
+      foreach(AHAddress c in _con_table.GetConnections(ConType.Near)) {
+	BigInteger dist = local.DistanceTo(c);
+	if( shorts == 0 ) {
+	  //This is the first one
+	  least_dist = dist;
+	  greatest_dist = dist;
+	}
+	else {
+	  if( dist > greatest_dist ) {
+	    greatest_dist = dist;
+	  }
+	  if( dist < least_dist ) {
+	    least_dist = dist;
+	  }
+	} 
+	shorts++;
+      }
+      /*
+       * Now we have the distance between the range of our neighbors
+       */
+      if( shorts > 0 ) {
+	if ( greatest_dist > least_dist ) {
+	  BigInteger width = greatest_dist - least_dist;
+	  //Here is our estimate of the inverse density:
+	  BigInteger inv_density = width/(shorts);
+	  //The density times the full address space is the number
+	  BigInteger full = Brunet.Address.Full;
+	  BigInteger total = full / inv_density;
+	  int total_int = total.IntValue();
+	  if( total_int > net_size ) {
+	    net_size = total_int;
+          }
+	}
+      }
+      _network_size = net_size;
+    }
+
     public bool AddConnection(SimNode other, ConType t) {
-      if (_con_table.Add(other.Address, t)) {
+      if (_con_table.Add(other.LocalAddress, t)) {
 	other.AddConnection(this, t);
+	EstimateSize();
 	return true;
       } else {
 	return false;
       }
     }
     
-    public int GetCount(ConType t) {
-      return _con_table.GetCount(t);
-    }
-    public int GetTotalCount() {
-      return _con_table.GetTotalCount();
-    }
-    public ArrayList GetConnections() {
-      return _con_table.GetConnections();
-    }
-      
     public AHAddress NextHop(AHAddress from, SimPacket p, out bool deliverlocally) {
       AHAddress next_node = null;  
       deliverlocally = false;
@@ -184,7 +244,7 @@ namespace Brunet {
       if( p.Hops > SimPacket.MAX_TTL) {
 	next_node = null;
       }
-      else if ( Address.Equals(dest) ) {
+      else if ( LocalAddress.Equals(dest) ) {
 	deliverlocally = true;
 	next_node = null;
       }
@@ -236,7 +296,7 @@ namespace Brunet {
 	    other_dist = l_dist;
 	  }
 	  if (p.Mode == RoutingMode.Greedy) {
-	    BigInteger our_dist = dest.DistanceTo(this.Address).abs();
+	    BigInteger our_dist = dest.DistanceTo(this.LocalAddress).abs();
 	    if( closest_dist < our_dist ) {
 	      if( closest_node != from ) {
 		next_node = closest_node;
@@ -252,7 +312,7 @@ namespace Brunet {
 	  else //other routing modes 
 	  {
 	    //Compute our left neighbor.
-	    int our_idx = _con_table.IndexOf(this.Address);
+	    int our_idx = _con_table.IndexOf(this.LocalAddress);
 	    if( our_idx < 0 ) {
 	      our_idx = ~our_idx;
 	    }
@@ -266,7 +326,7 @@ namespace Brunet {
 	       */
 	      deliverlocally = true;
 	      //The next step should be the node on the "other side"
-	      if( this.Address.IsLeftOf( dest ) ) {
+	      if(this.LocalAddress.IsLeftOf( dest ) ) {
 		next_node = right_n;
 	      }
 	      else {
@@ -361,7 +421,7 @@ namespace Brunet {
 	} //End of the case where we had to find a near route
       }
       if(p.Mode == RoutingMode.Exact) {
-	if( this.Address.Equals(dest) ) {
+	if(this.LocalAddress.Equals(dest) ) {
           deliverlocally = true;
           next_node = null;
         }
@@ -371,14 +431,11 @@ namespace Brunet {
       }
       return next_node;
     }
-  }
-
-
-  public class Simulator {
-    protected static Random _rand = new Random();
-
-    public static AHAddress ComputeShortcutTarget(Address local_address, int network_size) {
-      double logN = (double)(Address.MemSize * 8);
+    
+    public AHAddress ComputeShortcutTarget() {
+      AHAddress local_address = this.LocalAddress;
+      int network_size = _network_size;
+      double logN = (double)(Brunet.Address.MemSize * 8);
       double logk = Math.Log( (double) network_size, 2.0 );
       double p = _rand.NextDouble();
       double ex = logN - (1.0 - p)*logk;
@@ -407,6 +464,11 @@ namespace Brunet {
       BigInteger target_int = new BigInteger(t_add % Address.Full);
       return new AHAddress(target_int);
     }
+  }
+
+
+  public class Simulator {
+    protected static Random _rand = new Random();
     public static int ComputeOverlap(ArrayList list1, ArrayList list2) {
       int count = 0;
       foreach(AHAddress x in list1) {
@@ -450,14 +512,14 @@ namespace Brunet {
 	if (authorizer.Allow(node_1, node_2)) {
 	  node_1.AddConnection(node_2, ConType.Near);
 	} else {
-	  //Console.WriteLine("Cannot add near: {0} <-> {1}", node_1.Address, node_2.Address);
+	  //Console.WriteLine("Cannot add near: {0} <-> {1}", node_1.LocalAddress, node_2.LocalAddress);
 	  missing_near += 1;
 	}
 
 	if (authorizer.Allow(node_1, node_3)) {
 	  node_1.AddConnection(node_3, ConType.Near);
 	} else {
-	  //Console.WriteLine("Cannot add near: {0} <-> {1}", node_1.Address, node_3.Address);
+	  //Console.WriteLine("Cannot add near: {0} <-> {1}", node_1.LocalAddress, node_3.LocalAddress);
 	  missing_next_near += 1;
 	}
       }
@@ -469,13 +531,14 @@ namespace Brunet {
       // Place shortcuts.
       //
       int max_sc = (int) Math.Log( (double) network_size, 2.0 );
-      for (int iter = 0; iter < 2*max_sc; iter += 1) {
+      for (int iter = 0; iter < 10*max_sc; iter += 1) {
+	Console.WriteLine("iter: {0}", iter);
 	foreach (SimNode n in address_to_node.Values) {
 	  int logk = (int) Math.Log( (double) network_size, 2.0 );
-	  if (n.GetCount(ConType.Shortcut) > logk) {
+	  if (n.ConnectionTable.GetCount(ConType.Shortcut) > logk) {
 	    continue;
 	  }
-	  AHAddress t_addr = ComputeShortcutTarget(n.Address, network_size);
+	  AHAddress t_addr = n.ComputeShortcutTarget();
 	  int idx = sorted_node_list.BinarySearch(t_addr);
 	  if (idx < 0) {
 	    idx = ~idx;
@@ -485,16 +548,90 @@ namespace Brunet {
 	  if (authorizer.Allow(n, t_node)) {
 	    n.AddConnection(t_node, ConType.Shortcut);
 	  } else {
-	    //Console.WriteLine("Cannot add shortcut: {0} <-> {1}", n.Address, t_node.Address);
+	    //Console.WriteLine("Cannot add shortcut: {0} <-> {1}", n.LocalAddress, t_node.LocalAddress);
 	  }
 	}
       }
 
+      Console.WriteLine("Shortcuts added.");
+      ArrayList shortcut_dist = new ArrayList();
+      //
+      // Study the distribution of shortcut connections.
+      //
+      foreach(SimNode n in address_to_node.Values) {
+	int my_sorted_idx = sorted_node_list.BinarySearch(n.LocalAddress);
+	foreach(AHAddress c in n.ConnectionTable.GetConnections(ConType.Shortcut)) {
+	  int your_sorted_idx = sorted_node_list.BinarySearch(c);
+	  int d;
+	  if (my_sorted_idx > your_sorted_idx) {
+	    int temp = my_sorted_idx;
+	    my_sorted_idx = your_sorted_idx;
+	    your_sorted_idx = temp;
+	  }
+	  //your_sorted_idx greater than my_sorted_idx
+	  d = your_sorted_idx - my_sorted_idx;
+	  if (d > sorted_node_list.Count/2) {
+	    d = sorted_node_list.Count - d;
+	  }	  
+
+	  int idx = shortcut_dist.BinarySearch(d);
+	  if (idx < 0) {
+	    idx = ~idx;
+	  }
+	  shortcut_dist.Insert(idx, d);
+	}
+      }
+      //
+      // Compute the distance to which each shortcut goes.
+      //
+      ArrayList shortcut_prob = new ArrayList();
+      ArrayList shortcut_cumul = new ArrayList();
+      int curr_dist = -1;
+      int curr_count = 0;
+      int cumul_count = 0;
+      foreach(int d in shortcut_dist) {
+	if (curr_dist < 0) {
+	  curr_dist = d;
+	  curr_count = 1;
+	  cumul_count = 1;
+	  continue;
+	}
+	if (d == curr_dist) {
+	  curr_count += 1;
+	  cumul_count += 1;
+	  continue;
+	}
+	if (d != curr_dist) {
+	  Trace.WriteLineIf(d > curr_dist, "Error in distance cumulative distribution");
+	  shortcut_prob.Add(new object[] {curr_dist, curr_count});
+	  shortcut_cumul.Add(new object[] {curr_dist, cumul_count});
+	  curr_dist = d;
+	  curr_count = 1;
+	  cumul_count += 1;
+	}
+      }
+      if (curr_dist > 0) {
+	shortcut_prob.Add(new object[] {curr_dist, curr_count});	
+	shortcut_cumul.Add(new object[] {curr_dist, cumul_count});	
+      }
+
+      Console.WriteLine("Computing probability distribution");
+      foreach(object[] o in shortcut_prob) {
+	Console.WriteLine("{0} {1}", (int) o[0], (int) o[1]);
+      }
+
+      Console.WriteLine("Computing cumulative distribution");
+      foreach(object[] o in shortcut_cumul) {
+	Console.WriteLine("{0} {1}", (int) o[0], (int) o[1]);
+      }
+
+
+      Console.WriteLine("Analysing connections per node");
       double sum = 0.0;
       int max_con = 0;
       Hashtable dist = new Hashtable();
       foreach(SimNode n in address_to_node.Values) {
-	int count = n.GetTotalCount();
+	int count = n.ConnectionTable.GetTotalCount();
 	sum += count;
 	if (count > max_con) {
 	  max_con = count;
@@ -507,7 +644,7 @@ namespace Brunet {
  	}
       }
       for (int i = 0; i <= max_con; i++) {
-	//Console.WriteLine("{0} {1}", i, dist.ContainsKey(i)? dist[i]:0);
+	Console.WriteLine("{0} {1}", i, dist.ContainsKey(i)? dist[i]:0);
       }
 
       //
@@ -520,22 +657,26 @@ namespace Brunet {
 	SimNode node_3 = (SimNode) address_to_node[(AHAddress) sorted_node_list[(i + 2)%network_size]];
 	//check n1 and n2 for the strength of overlap
 	if (!authorizer.Allow(node_1, node_2)) {
-	  int first = ComputeOverlap(node_1.GetConnections(), node_2.GetConnections());
+	  int first = ComputeOverlap(node_1.ConnectionTable.GetAllConnections(), node_2.ConnectionTable.GetAllConnections());
 	  Console.WriteLine("first overlap: {0}", first);
 	}
 
 	//check n1 and n3 for the strength of overlap
 	if (!authorizer.Allow(node_1, node_3)) {
-	  int second = ComputeOverlap(node_1.GetConnections(), node_3.GetConnections());
+	  int second = ComputeOverlap(node_1.ConnectionTable.GetAllConnections(), node_3.ConnectionTable.GetAllConnections());
 	  Console.WriteLine("second overlap: {0}", second);
 	}
       }
 
 
+      //
+      // Analyse exact routing.
+      //
+      Console.WriteLine("Analysing exact routing");
       int broken_routes = 0;
       foreach (SimNode n1 in address_to_node.Values) {
 	foreach (SimNode n2 in address_to_node.Values) {
-	  SimPacket p = new SimPacket(n1.Address, n2.Address, RoutingMode.Exact);
+	  SimPacket p = new SimPacket(n1.LocalAddress, n2.LocalAddress, RoutingMode.Exact);
 	  SimNode curr = n1;
 	  AHAddress prev_addr = null;
 	  while (true) {
@@ -544,7 +685,7 @@ namespace Brunet {
 	    if (next == null) {
 	      break;
 	    }
-	    prev_addr = curr.Address;
+	    prev_addr = curr.LocalAddress;
 	    curr = (SimNode) address_to_node[next];
 	    p.Hops += 1;
 	  }
@@ -553,7 +694,49 @@ namespace Brunet {
 	  }
 	}
       }
-      Console.WriteLine("Broken routes: {0}", broken_routes);
+      Console.WriteLine("Broken paths for exact: {0}", broken_routes);
+      Console.WriteLine("Analysing greedy routing");      
+      //
+      // Analyse greedy routing for keys. (large number of keys).
+      //
+      int inconsistent = 0;
+      for (int i = 0; i < 10000; i++) {
+	AHAddress key_target = new AHAddress(new RNGCryptoServiceProvider());
+	if (i%100 == 0) {
+	  Console.WriteLine("{0}: {1} ({2} inconsistent)", i, key_target, inconsistent);
+	}
+	// source the key at different nodes in the network.
+	AHAddress target = null;
+	foreach (SimNode n in address_to_node.Values) {
+	  SimPacket p = new SimPacket(n.LocalAddress, key_target, RoutingMode.Greedy);
+	  SimNode curr = n;
+	  AHAddress prev_addr = null;
+	  while (true) {
+	    bool deliverlocally;
+	    AHAddress next = curr.NextHop(prev_addr, p, out deliverlocally);
+	    if (deliverlocally) {
+	      Trace.WriteLineIf(next != null, "Error in greedy routing. Next hop following delivery");
+	      if (target == null) {
+		target = curr.LocalAddress;
+	      } else {
+		if (target != curr.LocalAddress) {
+		  inconsistent++;
+		}
+	      }
+	      break;
+	    }
+
+	    if (next == null) {
+	      break;
+	    }
+	    
+	    prev_addr = curr.LocalAddress;
+	    curr = (SimNode) address_to_node[next];
+	    p.Hops += 1;
+	  }
+	}
+      }
+      Console.WriteLine("Inconsistently routed keys: {0}", inconsistent);
     }
   }
 }
