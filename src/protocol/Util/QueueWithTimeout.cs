a38e282bc8f30aef044be7849591e3c4436e17dd
davidiw@davidiw-desktop <davidiw@davidiw-desktop>
2007-09-10 17:50:46 -0400

    Added licenses for Dht and made it so that others could access ConnectionPacketHandler outside of Rpc

--- src/protocol/Util/QueueWithTimeout.cs
+++ src/protocol/Util/QueueWithTimeout.cs
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2005-2007  P. Oscar Boykin <boykin@pobox.com>, University of Florida
+Copyright (C) 2007  David Wolinsky <davidiw@ufl.edu>, University of Florida
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
