80e74e00baf7f6e46339ca8dfa1c40ae0195f241
David Wolinsky <davidiw@ufl.edu>
2008-06-30 11:56:08 -0400

    Should be mkbundle2 not mkbundle

--- scripts/basicnode_mkbundle.sh
+++ scripts/basicnode_mkbundle.sh
@@ -1,1 +1,1 @@
-mkbundle -o basicnode --deps --config-dir . --static -z BasicNode.exe Brunet.Dht.dll Brunet.dll CookComputing.XmlRpcV2.dll Brunet.DhtService.dll Brunet.XmlRpc.dll Brunet.Coordinate.dll
+mkbundle2 -o basicnode --deps --config-dir . --static -z BasicNode.exe Brunet.Dht.dll Brunet.dll CookComputing.XmlRpcV2.dll Brunet.DhtService.dll Brunet.XmlRpc.dll Brunet.Coordinate.dll
