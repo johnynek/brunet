e1c55174289357e777d80b9e71d3ecdcef483470
David Wolinsky <davidiw@ufl.edu>
2008-07-29 12:02:50 -0400

    Shutdown will now work on planetlab (ctrl-c / sigint)

--- scripts/planetlab/node/start_node.sh
+++ scripts/planetlab/node/start_node.sh
@@ -7,6 +7,8 @@ export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib:/home/$PLAB_ACCOUNT/node
 
 echo "Attempting to start basicnode"
 cd /home/$PLAB_ACCOUNT/node
-export MONO_NO_SMP=1; /home/$PLAB_ACCOUNT/node/basicnode /home/$PLAB_ACCOUNT/node/node.config.$PLAB_ACCOUNT 2>&1 | /home/$PLAB_ACCOUNT/node/cronolog --period="1 day" /home/$PLAB_ACCOUNT/node/node.log.%y%m%d.txt &
+export MONO_NO_SMP=1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`
+/home/$PLAB_ACCOUNT/node/basicnode /home/$PLAB_ACCOUNT/node/node.config.$PLAB_ACCOUNT 2>&1 | /home/$PLAB_ACCOUNT/node/cronolog --period="1 day" /home/$PLAB_ACCOUNT/node/node.log.%y%m%d.txt &
 # users will need to configure this themselves n server.py ... don't steal pre-allocated ports!
 python /home/$PLAB_ACCOUNT/node/server.py `whoami` &
