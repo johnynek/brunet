a3e54cb3af64d6f47fa9a2af07714d11ba8a10d8
David Wolinsky <davidiw@ufl.edu>
2009-07-12 16:33:33 -0400

    Improved scripts for the GroupVPN alpha

--- scripts/install_arch.sh
+++ scripts/install_arch.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
+if [[ ! -e `which tunctl 2> /dev/null` ]]; then
+  echo "Missing tunctl -- install uml-utilities"
+  exit
+elif [[ ! -e `which mono 2> /dev/null` ]]; then
+  echo "Missing mono -- install mono"
+  exit
+fi
+
 path=`which $0`
 path=`dirname $path`
-cd $path
+cd $path &> /dev/null
+
 mkdir -p /opt/ipop/bin
-cp ../bin/*py /opt/ipop/bin/.
-cp ../bin/*exe /opt/ipop/bin/.
-cp ../bin/*sh /opt/ipop/bin/.
+cp ./bin/*py /opt/ipop/bin/.
+cp ./bin/*exe /opt/ipop/bin/.
+cp ./bin/*sh /opt/ipop/bin/.
 
 mkdir -p /opt/ipop/lib
-cp ../bin/*dll /opt/ipop/lib/.
-cp ../bin/*so /opt/ipop/lib/.
+cp ./bin/*dll /opt/ipop/lib/.
+cp ./bin/*so /opt/ipop/lib/.
 
 mkdir -p /opt/ipop/etc
-cp ../config/ipop.vpn.config /opt/ipop/etc/.
-cd -
+cp ./config/ipop.vpn.config /opt/ipop/etc/.
+
+cd - &> /dev/null
 
 mkdir -p /opt/ipop/var
 
-ln -s /opt/ipop/bin/groupvpn_prepare.sh /usr/sbin/.
-ln -s /opt/ipop/bin/ipop_linux.sh /etc/rc.d/ipop_linux.sh
-ln -s /opt/ipop/etc/ipop.vpn.config /etc/.
+ln -sf /opt/ipop/bin/groupvpn_prepare.sh /usr/sbin/.
+ln -sf /opt/ipop/bin/ipop_linux.sh /etc/rc.d/ipop_linux.sh
+ln -sf /opt/ipop/etc/ipop.vpn.config /etc/.
+
+echo "Done installing IPOP"
