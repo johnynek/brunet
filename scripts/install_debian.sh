9611f2735c44bbba7f13522881617143ff282617
David Wolinsky <davidiw@ufl.edu>
2009-07-27 15:15:26 -0400

    Debian script had an Arch artifact

--- scripts/install_debian.sh
+++ scripts/install_debian.sh
@@ -34,7 +34,7 @@ mkdir -p /opt/ipop/var
 
 ln -sf /opt/ipop/bin/groupvpn_prepare.sh /usr/sbin/.
 ln -sf /opt/ipop/bin/ipop_linux.sh /etc/init.d/ipop_linux.sh
-ln -sf /opt/ipop/bin/bootstrap_linux.sh /etc/rc.d/bootstrap_linux.sh
+ln -sf /opt/ipop/bin/bootstrap_linux.sh /etc/init.d/bootstrap_linux.sh
 ln -sf /opt/ipop/etc/ipop.vpn.config /etc/.
 
 echo "Done installing IPOP"
