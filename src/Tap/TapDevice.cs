f31ba5f0d21772d0e87cffbcfaa0cc9a015831a1
David Wolinsky <davidiw@ufl.edu>
2009-07-27 15:17:56 -0400

    Added WindowsTap.cs, hard work was done by Pierre A C# driver interface for the Windows TAP, no longer necessary to compile a libtuntap.dll for Windows 32-bit and 64-bit.

--- src/Tap/TapDevice.cs
+++ src/Tap/TapDevice.cs
@@ -1,5 +1,24 @@
+/*
+Copyright (C) 2009  David Wolinsky <davidiw@ufl.edu>, University of Florida
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 using Brunet;
 using Brunet.Applications;
+using System;
 
 namespace Ipop.Tap {
   public abstract class TapDevice {
@@ -9,7 +28,7 @@ namespace Ipop.Tap {
       if(OSDependent.OSVersion == OSDependent.OS.Linux) {
         tap = new LinuxTap(device_name);
       } else if(OSDependent.OSVersion == OSDependent.OS.Windows) {
-        tap = new cTap(device_name);
+        tap = new WindowsTap(device_name);
       } else {
         tap = new cTap(device_name);
       }
