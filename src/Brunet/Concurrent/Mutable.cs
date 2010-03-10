e3ae8c31e1cfe8f4fbb7452bb9507828bdaf6aaa
P. Oscar Boykin <boykin@pobox.com>
2010-03-10 13:00:56 -0500

    Documentation change * Updated README files to describe modules and DEPENDENCIES

--- src/Brunet/Concurrent/Mutable.cs
+++ src/Brunet/Concurrent/Mutable.cs
@@ -20,7 +20,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 using NUnit.Framework;
 #endif
 
-using Brunet.Util;
 using Brunet.Collections;
 using System.Threading;
 
--- src/Brunet/Util/PType.cs
+++ src/Brunet/Util/PType.cs
@@ -22,8 +22,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 using NUnit.Framework;
 #endif
 
-using Brunet.Util;
-
 namespace Brunet.Util {
 
 #if BRUNET_NUNIT
