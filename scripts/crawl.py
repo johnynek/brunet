2705718d871b09b86234e500b3a14247c88fdcfd
David Wolinsky <davidiw@ufl.edu>
2010-05-11 18:45:37 -0400

    Wrote a multi-threaded crawler - Complete crawl in 21 seconds vs 5 to 10 minutes using sequential crawler - More detailed accuracy of consistency, since ordering is slightly more randomized

--- scripts/crawl.py
+++ scripts/crawl.py
@@ -1,57 +1,38 @@
-#!/usr/bin/python
-""" This crawls the brunet namespace using Information.Info.  During crawling it
-also determines if the ring is consistent, does a node agree with its two
-neighbors on a given side about their positioning.  This can be used by other
-python programs if they call crawl and use the nodes that are returned. """
-import xmlrpclib, pybru, sys, getopt, time
+#!/usr/bin/env python
+import xmlrpclib, sys, time, thread, types, getopt, pybru
 
 usage = """usage:
-python crawl.py [--debug] [--debug2] [--port=<xmlrpc port of a brunet node>]
+python crawl.py [--debug] [--port=<int>] [--namespace=<string>]
 debug = print the current node crawling
-debug2 = debug + print the neighbors of current node
 port = the xmlrpc port for a brunet node to be used for crawling
+namespace = the brunet namespace to crawl
 help = this message"""
 
-# Default starting point
 def main():
   try:
-    optlist, args = getopt.getopt(sys.argv[1:], "", ["debug", "port=", "debug2"])
+    optlist, args = getopt.getopt(sys.argv[1:], "", ["debug", "port=", "namespace="])
 
     logger = null_logger
     port = 10000
-    debug = False
+    ns = ""
 
     for k,v in optlist:
       if k == "--port":
         port = int(v)
       elif k == "--debug":
         logger = print_logger
-      elif k == "--debug2":
-        logger = print_logger
-        debug = True
+      elif k == "--namespace":
+        ns = v
   except:
     print usage
     return
 
-  nodes = crawl(port, logger, debug)
-  count, consistency = check_results(nodes)
-
-  print "Consistent Nodes: " + str(consistency)
-
-  cons = 0
-  if consistency != 0:
-    cons = consistency / (count * 1.0)
-  print "Consistency: " + str(cons)
-
-def check_results(nodes):
-  count = 0
-  consistency = 0.0
-
-  for addr in nodes:
-    count += 1
-    consistency += nodes[addr]['consistency']
-
-  return consistency, count
+  crawler = Crawler(port, logger, ns)
+  start = time.time()
+  crawler.start()
+  crawler.print_results()
+  end = time.time()
+  print "Crawl took: " + str(end - start)
 
 def print_logger(msg):
   print msg
@@ -59,115 +40,237 @@ def print_logger(msg):
 def null_logger(msg):
   pass
 
-# crawls the network using the xmlrpc at the specified port.  Logger is a
-# generic function / method thats only parameter is a single string which
-# contains some logging output.  This file provides print_logger which calls
-# print on the msg and null logger which doesn't do anything.
-#
-# @todo currently this script does not handle nodes not having left2 and right2
-# and nodes will not be skipped until the set of four (right, right2, left,
-# left2).  This could make the crawlers results slightly wrong.
-def crawl(port = 10000, logger = null_logger, debug = False):
-  port = str(port)
-  #gain access to the xmlrpc server
-  rpc = xmlrpclib.Server("http://127.0.0.1:" + port + "/xm.rem")
-  #a list of nodes we have looked up
-  nodes = {}
+class Crawler:
+  def __init__(self, port = 10000, logger = null_logger, ns = ""):
+    if not port is types.IntType:
+      pass
 
-  #getting start node
-  node = rpc.localproxy("sys:link.GetNeighbors")['self']
-  start = pybru.Address(node)
-  last = node
-  #maximum times of going back one before failing
-  retry_max = 3
-  no_response_max = 3
+    self.logger = logger
+    port = str(port)
+    path = "xm.rem"
 
-  no_response_count = 0
-  retry_count = 0
-  #after the transition from node 0->Z, we've visted all nodes less than us
-  half_way = False
+    if ns != "":
+      #gain access to the xmlrpc server
+      rpc = xmlrpclib.Server("http://127.0.0.1:" + port + "/xmserver.rem")
+      nodes = rpc.listNodes()
+      path = ""
+      for node in nodes:
+        #All nodes export themselves as namespace:node_address
+        if node[:len(ns)] == ns:
+          path = node
+          break
 
-  while True:
-    try:
-      logger(node + " " + str(retry_count) + " " + str(no_response_count) + "\n")
-      res = rpc.proxy(node, 3, 1, "Information.Info")[0]
-      if debug:
-        logger(str(res))
-      neighbors = res['neighbors']
-      info = {}
-      info['right'] = neighbors['right']
-      info['left'] = neighbors['left']
+      if path == "":
+        print "Could not find a matching namespace"
+        pass
 
+    self.rpc = xmlrpclib.Server("http://127.0.0.1:" + port + "/" + path)
+    # access to the "nodes" table
+    self.lock = thread.allocate_lock()
+    # sort of like a "join" statement
+    self.done = thread.allocate_lock()
+    self.nodes = {}
+
+  def start(self):
+    node = self.rpc.localproxy("sys:link.GetNeighbors")['self']
+    start = pybru.Address(node)
+    # we're starting, we'll wait for the last guy to release this lock
+    self.done.acquire()
+    self.threads = 1
+    thread.start_new_thread(self.run, (node,))
+    # waiting on the last guy
+    self.done.acquire()
+    self.consistency()
+
+  def run(self, node):
+    attempts = 3
+
+    # query the node and parse the results
+    while attempts > 0:
       try:
-        info['right2'] = neighbors['right2']
+        res = self.rpc.proxy(node, 3, 1, "Information.Info")
       except:
-        info['right2'] = ""
+        # Means the remote side didn't support the operation, empty means no response
+        res = {"type": "bad user"}
+        try:
+          res["neighbors"] = self.rpc.proxy(node, 3, 1, "sys:link.GetNeighbors")[0]
+        except:
+          res["neighbors"] = {}
+        res = [res]
+      self.logger(str(res))
+      (peers, info) = self.parse(res)
+      if peers != [] or info != {}:
+        break;
+      attempts -= 1
 
-      try:
-        info['left2'] = neighbors['left2']
-      except:
-        info['left2'] = ""
 
-      ip_list = res['localips']
+    to_query = []
+
+    # add the results to the table and determine any new nodes that need to be queried
+    self.lock.acquire()
+    self.nodes[node] = info
+    for neighbor in peers:
+      if neighbor in self.nodes:
+        continue
+      to_query.append(neighbor)
+      self.nodes[neighbor] = True
+
+    self.nodes[node] = info
+    if to_query == 0:
+      # no new nodes, we're done
+      self.threads -= 1
+    else:
+      # add as many new threads as nodes, except the current thread still is working
+      self.threads += len(to_query) - 1
+
+    if self.threads == 0:
+      # last thread, notifies the main thread of completion
+      self.done.release()
+    self.logger("Threads remaining: " + str(self.threads))
+
+    self.lock.release()
+
+    # start new threads outside the lock
+    for idx in range(len(to_query) - 1):
+      thread.start_new_thread(self.run, (to_query[idx], ))
+
+    # this is the thread starter executing
+    if len(to_query) > 0:
+      self.run(to_query[len(to_query) - 1])
+
+    # when the thread gets here, its completely done
+    thread.exit()
+
+  # parse the result of an rpc
+  def parse(self, node_info):
+    # if no data, return empty sets
+    if len(node_info) == 0:
+      return ([], {})
+    node_info = node_info[0]
+    neighbors = node_info['neighbors']
+
+    peers = []
+    for k, v in neighbors.iteritems():
+      if k == "self":
+        continue
+      peers.append(v)
+
+    info = {}
+    info['right'] = neighbors['right'] if 'right' in neighbors else ''
+    info['right2'] = neighbors['right2'] if 'right2' in neighbors else ''
+    info['left'] = neighbors['left'] if 'left' in neighbors else ''
+    info['left2'] = neighbors['left2'] if 'left2' in neighbors else ''
+
+    if 'localips' in node_info:
+      ip_list = node_info['localips']
       ips = ""
       for ip in ip_list:
         ips += ip + ", "
       ips = ips[:-2]
       info['ips'] = ips
+    else:
+      info['ips'] = ""
 
-      info['geo_loc'] = res['geo_loc']
-      info['type'] = res['type']
-      if info['type'] == "IpopNode":
-        try:
-          info['virtual_ip'] = res['Virtual IP']
-        except:
-          info['virtual_ip'] = ""
-        info['namespace'] = res['IpopNamespace']
-      info['retries'] = no_response_count * (retry_count + 1)
+    info['type'] = node_info['type'] if 'type' in node_info else ''
+    info['virtual_ip'] = node_info['VirtualIPs'][0] if 'VirtualIPs' in node_info else ''
+    info['namespace'] = node_info['IpopNamespace'] if 'IpopNamespace' in node_info else ''
+    info['retries'] = 0
+    info['cons'] = node_info['cons'] if 'cons' in node_info else 0
+    info['tcp'] = node_info['tcp'] if 'tcp' in node_info else 0
+    info['tunnel'] = node_info['tunnel'] if 'tunnel' in node_info else 0
+    info['udp'] = node_info['udp'] if 'udp' in node_info else 0
+    info['subring'] = node_info['subring'] if 'subring' in node_info else 0
+    info['sas'] = node_info['sas'] if 'sas' in node_info else 0
+    info['wedges'] = node_info['wedges'] if 'wedges' in node_info else 0
 
-      no_resonse_count = 0
-      if node != last:
-        retry_count = 0
-    except:
-      no_response_count += 1
-      if no_response_count == no_response_max:
-        node = last
-        no_response_count = 0
-        retry_count += 1
-        if retry_count ==  retry_max:
-          print "Unable to crawl the system."
-          break
-      continue
-    #it is possible that the node we're trying to talk to is the one we end up with!
-    node = neighbors['self']
-    #Once we've visited all nodes less than us, we shouldn't see another until
-    #we're done crawling
+    return (peers, info)
 
-    if pybru.Address(node) > start:
-      half_way = True
-    elif half_way and pybru.Address(node) <= start:
-      break
-    elif pybru.Address(node) == start and len(nodes) > 1:
-      half_way = True
-      break
+  def print_results(self):
+    res = self.aggregate()
+    print "Total Nodes: " + str(res[0])
+    print "Consistent Nodes: " + str(res[1])
 
-    #maintain a list of everyones neighbors
-    nodes[node] = info
-    last = node
-    node = info['right']
+    cons = 0
+    if res[0] != 0:
+      cons = res[1] + res[2] + res[3] + res[4]
+      cons = cons / (res[0] * 4.0)
+    print "Consistency: " + str(cons)
+    print "Connections: " + str(res[6])
+    print "SecurityAssociations: " + str(res[7])
+    print "WrappedEdges: " + str(res[8])
+    print "Edges: " + str(res[5])
+    print "RelayEdges: " + str(res[10])
+    print "SubringEdges: " + str(res[12])
+    print "TcpEdges: " + str(res[9])
+    print "UdpEdges: " + str(res[11])
 
-  for addr in nodes:
-    node = nodes[addr]
-    lcons = 0.0
-    if(node['left'] in nodes and nodes[node['left']]['right'] == addr):
-      lcons += 1.0
-    if(node['left2'] in nodes and nodes[node['left2']]['right2'] == addr):
-      lcons += 1.0
-    if lcons > 0:
-      lcons /= 2.0
-    nodes[addr]['consistency'] = lcons
+  def consistency(self):
+    for addr in self.nodes:
+      con = self.consis(addr)
+      self.nodes[addr]['consistency'] = (con[0] + con[1] + con[2] + con[3]) / 4.0
 
-  return nodes
+  def consis(self, addr):
+    node = self.nodes[addr]
+
+    lcon = 1 if('right' in node and \
+        node['right'] in self.nodes and \
+        'left' in self.nodes[node['right']] and \
+        self.nodes[node['right']]['left'] == addr) \
+        else 0
+    rcon = 1 if('left' in node and \
+        node['left'] in self.nodes and \
+        'right' in self.nodes[node['left']] and \
+        self.nodes[node['left']]['right'] == addr) \
+        else 0
+    lcon2 = 1 if('left2' in node and \
+        node['left2'] in self.nodes and \
+        'right2' in self.nodes[node['left2']] and \
+        self.nodes[node['left2']]['right2'] == addr) \
+        else 0
+    rcon2 = 1 if('right2' in node and \
+        node['right2'] in self.nodes and \
+        'left2' in self.nodes[node['right2']] and \
+        self.nodes[node['right2']]['left2'] == addr) \
+        else 0
+
+    return (lcon, rcon, lcon2, rcon2)
+
+  def aggregate(self):
+    count = 0
+    r1consistency = 0
+    r2consistency = 0
+    l1consistency = 0
+    l2consistency = 0
+    sas = 0
+    cons = 0
+    tunnel = 0
+    udp = 0
+    tcp = 0
+    edges = 0
+    wedges = 0
+    subring = 0
+    for addr in self.nodes:
+      count += 1
+      consis = self.consis(addr)
+      l1consistency += consis[0]
+      r1consistency += consis[1]
+      l2consistency += consis[2]
+      r2consistency += consis[3]
+      sas += node['sas']
+      cons += node['cons']
+      wedges += node['wedges']
+      ltunnel = node['tunnel']
+      tunnel += ltunnel
+      ltcp = node['tcp']
+      tcp += ltcp
+      ludp = node['udp']
+      udp += ludp
+      lsubring = node['subring']
+      subring += lsubring
+      edges += ltunnel + ludp + ltcp + lsubring
+      
+
+    return (count, r1consistency, r2consistency, l1consistency, l2consistency, edges, cons, sas, wedges, tcp, tunnel, udp, subring)
 
 if __name__ == "__main__":
   main()
