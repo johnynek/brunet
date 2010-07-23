c029bfcfffbb884c39ac1473e05e40b8382fe3c8
David Wolinsky <davidiw@ufl.edu>
2010-07-23 10:39:36 -0400

    Path roots don't create PathEdges, which was breaking the ability for UpdateLocalTAs

--- src/Brunet/Transport/PathEdgeListener.cs
+++ src/Brunet/Transport/PathEdgeListener.cs
@@ -617,6 +617,8 @@ namespace Brunet.Transport {
       PathEdge pe = e as PathEdge;
       if(pe != null) {
         _el.UpdateLocalTAs(pe.Edge, ta);
+      } else {
+        _el.UpdateLocalTAs(e, ta);
       }
     }
 
