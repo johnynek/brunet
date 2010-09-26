d6ce1891504a6f880f5117d36e17be0e28db2bee
David Wolinsky <davidiw@ufl.edu>
2010-09-26 00:05:17 -0400

    [Scripts] Reduced the timeout on dht proxy dump / load

--- scripts/dump_dht_proxy.py
+++ scripts/dump_dht_proxy.py
@@ -2,7 +2,7 @@
 import xmlrpclib, pickle, sys, socket 
 
 rpc = xmlrpclib.Server("http://127.0.0.1:10000/xm.rem")
-socket.setdefaulttimeout(60) 
+socket.setdefaulttimeout(15) 
 res = rpc.localproxy("RpcDhtProxy.ListEntries")
 f = open(sys.argv[1], 'w+')
 pickle.dump(res, f)
--- scripts/load_dht_proxy.py
+++ scripts/load_dht_proxy.py
@@ -2,7 +2,7 @@
 import xmlrpclib, pickle, sys, socket
 
 rpc = xmlrpclib.Server("http://127.0.0.1:10000/xm.rem")
-socket.setdefaulttimeout(60) 
+socket.setdefaulttimeout(15) 
 f = open(sys.argv[1], 'r')
 data = pickle.load(f)
 f.close()
